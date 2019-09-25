ac63ca7e2777b7f68a97bab243a5ece439458f4f
root <root@10.0.2.15>
2019-09-25 22:37:57 +0530

    klj

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 sudo yum install -y httpd
 sudo firewall-cmd --add-port=80/tcp --permanent
 sudo firewall-cmd --add-service=http --permanent
+sudo firewall-cmd --reload
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
